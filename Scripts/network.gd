5b2b36237b2e397385585015867c97cbb393a4e4
CogumeloGames <45271020+CogumeloGames@users.noreply.github.com>
2021-07-15 02:49:28 -0300

    Add files via upload

---
+++ Scripts/network.gd
@@ -0,0 +1,38 @@
+extends Node
+
+const DEFAULT_PORT = 28960
+const MAX_CLIENTS = 6
+
+var server = null
+var client = null
+
+var ip_adress
+
+func _ready() -> void:
+	if OS.get_name() == "Windows":
+		ip_adress == IP.get_local_adress()[3]
+	else:
+		ip_adress == IP.get_local_adress()[3]
+	
+	for ip in IP.get_local_addresses():
+		if ip.begins_with("192.168."):
+			ip_adress = ip
+	
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+	
+func create_server() -> void:
+	server = NetworkedMultiplayerENet.new()
+	server.create_server(DEFAULT_PORT, MAX_CLIENTS)
+	get_tree().set_network_peer(server)
+	
+func join_server() -> void:
+	server = NetworkedMultiplayerENet.new()
+	server.create_client(ip_adress, DEFAULT_PORT)
+	get_tree().set_network_peer(client)
+	
+func _connect_to_server() -> void:
+	print("Sucessfully connected to the server")
+	
+func _server_disconnected() -> void:
+	print("Disconnected from the server")
